e6f2bb1eec5b4aee9f0b512822f900fef354b379
Felipe Neves <rj.felipeneves@gmail.com>
2024-03-27 00:39:14 -0300

    Remoção do html

--- ex3.php
+++ ex3.php
@@ -1,39 +1,25 @@
-<!DOCTYPE html>
-<html lang="pt-br">
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>EXERCÍCIO 3 - DESAFIO DEV - PARES & ÍMPARES</title>
-</head>
+<?php
+function compare($a, $b)
+{
+  if ($a % 2 == 0 && $b % 2 != 0) {
+    return -1;
+  } elseif ($a % 2 != 0 && $b % 2 == 0) {
+    return 1;
+  } else {
+    return $a - $b;
+  }
+}
 
-<body>
-  <h1>
-    <?php
-    function compare($a, $b)
-    {
-      if ($a % 2 == 0 && $b % 2 != 0) {
-        return -1;
-      } elseif ($a % 2 != 0 && $b % 2 == 0) {
-        return 1;
-      } else {
-        return $a - $b;
-      }
-    }
+function sortOddEven($array)
+{
+  usort($array, "compare");
+  return $array;
+}
 
-    function sortOddEven($array)
-    {
-      usort($array, "compare");
-      return $array;
-    }
+// Coloque aqui o seu array (Input)
+$input = array(3, 1, 2, 4, 6, 5);
 
-    // Coloque aqui o seu array (Input)
-    $input = array(3, 1, 2, 4, 6, 5);
-
-    $result = sortOddEven($input);
-    echo "Output: " . implode(", ", $result);
-    ?>
-  </h1>
-</body>
-
-</html>
+$result = sortOddEven($input);
+echo "Output: " . implode(", ", $result);
+?>
